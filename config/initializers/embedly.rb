0b24c00efa2e2c4159afa7e94be1fa461898ea71
AppAcademyStudent <student@appacademy.io>
2014-05-28 18:05:52 -0700

    require figaro

--- config/initializers/embedly.rb
+++ config/initializers/embedly.rb
@@ -1,5 +1,6 @@
+require 'figaro'
 def initialize_embedly
-	Embedly::API.new({:key => Figaro.env.embedly_api_key}) 
+	Embedly::API.new({:key => Figaro.env.embedly_api_key})
 end
 
 $embedly_api = initialize_embedly
