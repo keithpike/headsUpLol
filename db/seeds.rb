7c599c82f9c5bff98a1cd4c05d3c6cf039171f7e
App Academy Student <student@appacademy.io>
2014-05-30 17:20:15 -0700

    style button

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
-Blog.create({owner_id: 1, title: 'hello', url: 'something.com'})
-Blog.create({owner_id: 1, title: 'test2', url: 'something2.com'})
-Post.create({blog_id: 1, post_type: 'Text'})
-Post.create({blog_id: 1, post_type: 'Text'})
-Post.create({blog_id: 1, post_type: 'Video'})
-Post.create({blog_id: 1, post_type: 'Video'})
